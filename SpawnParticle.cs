f0d667093978c99d8407e66141361d1a57b49217
Artem <lagmis-ar@mail.ru>
2020-05-29 21:42:08 +0700

    Add files via upload

---
+++ SpawnParticle.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class SpawnParticle : MonoBehaviour
+{
+    // prefab
+    public GameObject particle;
+
+    private void Start()
+    {
+        // Static particles
+        StartCoroutine (SpawnStatic ());
+
+        // Fast particles in move
+        for (int i = 0; i < 11; i++)
+        {
+            StartCoroutine (Spawn ());
+        }
+    }
+
+    IEnumerator SpawnStatic () {
+        for (int i = 0; i < 390; i++) {
+            Vector3 pos = new Vector3 (0, 0, 0);
+            float randScale = 0;
+
+            pos = new Vector3 (Random.Range (-6f, 27f), Random.Range (-3f, 13f), 10);
+            randScale = (Random.Range (0, 85) == 0) ? 3f : Random.Range (-0.8f, 1.2f);
+
+            GameObject newParticle = Instantiate (particle, pos, Quaternion.Euler (0, 0, Random.Range (0, 360))) as GameObject;
+
+            newParticle.transform.parent = gameObject.transform;
+            newParticle.transform.localPosition = pos;
+            newParticle.GetComponent <StarParticle> ().randScale = randScale;
+            newParticle.GetComponent <StarParticle> ().speed = -0.03f;
+
+            yield return new WaitForEndOfFrame (); // For cool Random.Range
+        }
+        yield break;
+    }
+
+    IEnumerator Spawn () {
+        Vector3 pos = new Vector3 (0, 0, 0);
+        float randScale = 0;
+
+        switch (Random.Range (0, 4)) { // + delay
+            case 0:
+                yield return new WaitForSeconds (1.5f);
+                break;
+            case 1:
+                yield return new WaitForSeconds (3f);
+                break;
+            case 3:
+                yield return new WaitForSeconds (4.5f);
+                break;
+        }
+
+        pos = new Vector3 (Random.Range (-6f, 6f), Random.Range (-3.2f, 3.2f), 10);
+        randScale = (Random.Range (0, 85) == 0) ? 2.6f : Random.Range (-0.6f, 1.2f);
+
+        GameObject newParticle = Instantiate (particle, pos, Quaternion.Euler (0, 0, Random.Range (0, 360))) as GameObject;
+
+        newParticle.transform.parent = gameObject.transform;
+        newParticle.transform.localPosition = pos;
+        newParticle.GetComponent <StarParticle> ().randScale = randScale;
+        newParticle.GetComponent <StarParticle> ().speed = Random.Range (1.5f, 10f);
+
+        while (true)
+        {
+            yield return new WaitForSeconds (6f);
+
+            pos = new Vector3 (Random.Range (-8f, 8f), Random.Range (-3.44f, 3.44f), 10);
+
+            newParticle.transform.localPosition = pos;
+            newParticle.transform.localRotation = Quaternion.Euler (0, 0, Random.Range (0, 360));
+        }
+    }
+}
---
+++ StarParticle.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class StarParticle : MonoBehaviour
+{
+    public float scale = 1f;
+    public float randScale;
+    public float speed;
+
+    private void Update()
+    {
+        if (speed == -0.03f) // for static stars
+            gameObject.transform.localPosition += new Vector3 (speed * Time.deltaTime, (speed / 2.5f) * Time.deltaTime, 0);
+        else
+            gameObject.transform.localPosition += gameObject.transform.up * Time.deltaTime * speed;
+
+        // this is need for animation of particles' scale with Unity Animation
+        gameObject.transform.localScale = new Vector3 (scale + randScale, scale + randScale, gameObject.transform.localScale.z);
+    }
+}
